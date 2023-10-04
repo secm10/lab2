60747d6b89249c1e39583a30d46df63ea2f9fee1
unknown <secm10>
2023-10-04 09:29:45 -0400

    lab_2parte2

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import Navbar from '@/components/Navbar'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Footer from '@/components/Footer'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header/>
         <Navbar/>
+        <Footer/>
         {children}
       </body>
     </html>
